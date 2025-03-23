a7824b7ce77c94f2187852b4aa77252be5f56840
fooble2137 <github@fooble.dev>
2025-03-23 12:27:20 +0100

    QR-Code Download Function

--- script.js
+++ script.js
@@ -8,15 +8,29 @@ generateBtn.addEventListener("click", () => {
   let qrValue = qrInput.value;
   if (!qrValue) return;
 
-  generateBtn.innerText = "Generating QR-Code...";
+  generateBtn.innerHTML = '<i class="ri-loop-left-line"></i> Generating QR-Code...';
 
   let url = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
 
   qrImg.src = url;
-  downloadBtn.href = url;
+  downloadBtn.addEventListener("click", () => download(url));
 
   qrImg.addEventListener("load", () => {
     wrapper.classList.add("active");
     generateBtn.innerText = "Generate QR-Code";
   });
 });
+
+async function download(url) {
+  const a = document.createElement("a");
+  a.href = await toDataURL(url);
+  a.download = "qrcode.png";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+async function toDataURL(url) {
+  const blob = await fetch(url).then(res => res.blob());
+  return URL.createObjectURL(blob);
+}
